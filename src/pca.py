78291c32e91a91f3f9a7276cbf6c597ab7120fc8
Jose Blanca <jblanca@upv.es>
2022-02-14 14:51:30 +0100

    tomato practice now uses the allel pca implementation

--- src/pca.py
+++ src/pca.py
@@ -3,6 +3,29 @@ import seaborn
 import allel
 
 
+def do_pca_from_012_genotypes(
+    genotypes,
+    n_components=10,
+    scaler=None,
+    use_randomized_pca_optimization=False,
+):
+
+    if use_randomized_pca_optimization:
+        pca_funct = allel.randomized_pca
+    else:
+        pca_funct = allel.pca
+    projections, model = pca_funct(genotypes, n_components=n_components, scaler=scaler)
+
+    explained_variances = model.explained_variance_ratio_
+    princom_names = [f"princomp_{idx + 1}" for idx in range(explained_variances.size)]
+    explained_variances = pandas.Series(explained_variances, index=princom_names)
+    explained_variances = explained_variances * 100.0
+
+    projections = pandas.DataFrame(projections, columns=princom_names)
+
+    return {"projections": projections, "explained_variances": explained_variances}
+
+
 def do_pca(
     genotypes,
     ld_pruning_params=None,
@@ -18,25 +41,17 @@ def do_pca(
     num_variants_after_ld_pruning = genotypes.num_variants
     genotypes = genotypes.to_genotype_012_matrix()
 
-    if use_randomized_pca_optimization:
-        pca_funct = allel.randomized_pca
-    else:
-        pca_funct = allel.pca
-    projections, model = pca_funct(genotypes, n_components=n_components, scaler=scaler)
+    result = do_pca_from_012_genotypes(
+        genotypes,
+        n_components=n_components,
+        scaler=scaler,
+        use_randomized_pca_optimization=use_randomized_pca_optimization,
+    )
 
-    explained_variances = model.explained_variance_ratio_
-    princom_names = [f"princomp_{idx + 1}" for idx in range(explained_variances.size)]
-    explained_variances = pandas.Series(explained_variances, index=princom_names)
-    explained_variances = explained_variances * 100.0
+    result["num_variants_after_ld_pruning"] = num_variants_after_ld_pruning
+    result["num_variants_before_ld_pruning"] = num_variants_before_ld_pruning
 
-    projections = pandas.DataFrame(projections, columns=princom_names)
-
-    return {
-        "projections": projections,
-        "explained_variances": explained_variances,
-        "num_variants_after_ld_pruning": num_variants_after_ld_pruning,
-        "num_variants_before_ld_pruning": num_variants_before_ld_pruning,
-    }
+    return result
 
 
 def plot_pca_result(
